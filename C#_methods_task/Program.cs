97b1283b40a9bb0dd7c5d1d08706731f18791a2e
Gasimoff0 <mddska.01@icloud.com>
2025-05-05 00:37:44 +0400

    Proje dosyası ekle.

---
+++ C#_methods_task/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        #region task1
+        //Console.WriteLine("Ededi daxil edin");
+        //int eded = Convert.ToInt32(Console.ReadLine());
+        //eded = KvadrataYukselt(eded);
+        //Console.WriteLine("ededin kvadrati: " + eded);
+        #endregion
+
+        #region task2
+        //int[] ededler = { 1, 2, 3, 4, 5 };
+        //int[] kvadratEdedler = KvadratlaraYukselt(ededler);
+
+        //Console.WriteLine("Kvadratlara yukselmish massiv:");
+        //foreach (int num in kvadratEdedler)
+        //{
+        //    Console.Write(num + " ");
+        //}
+        #endregion
+
+        #region task3     
+        //Console.WriteLine("Yazi daxil edin");
+        //string name = Console.ReadLine();
+        //name = BosluqlariSil(name);
+        //Console.WriteLine("Boshluqsuz yazi: " + name);
+        #endregion
+
+        #region task4
+        //int[] nums = { 1, 5, 7 };
+        //nums = ArrayElementElaveEt(nums, 3);
+
+        //Console.WriteLine("Yeni array:");
+        //foreach (int num in nums)
+        //{
+        //    Console.Write(num + " ");  
+        //}
+        #endregion
+
+    }
+    //task1
+    public static int KvadrataYukselt(int x)
+    {
+        return x * x;
+    }
+
+    //task2
+    public static int[] KvadratlaraYukselt(int[] array)
+    {
+        int[] yeniArray = new int[array.Length];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            yeniArray[i] = array[i] * array[i];
+        }
+
+        return yeniArray;
+    }
+
+    //task3
+    public static string BosluqlariSil(string metn)
+    {
+        return metn.Replace(" " , "");
+    }
+
+    //task4
+    public static int[] ArrayElementElaveEt(int[] array, int yeniElement)
+    {
+        int[] yeniArray = new int[array.Length + 1];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            yeniArray[i] = array[i];
+        }
+
+        yeniArray[array.Length] = yeniElement;
+
+        return yeniArray;
+    }
+}
